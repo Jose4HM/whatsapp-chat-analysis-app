82d0708f4d53b72e98ffdfe522a1b81587085729
Jose4HM <joshanccom@gmail.com>
2024-08-25 20:10:48 -0500

    Change datetime range

--- app.py
+++ app.py
@@ -98,8 +98,8 @@ df = df.dropna()
 df.reset_index(drop=True, inplace=True)
 
 # #### Filtrar el chat por fecha de acuerdo a lo requerido
-start_date = '2023-01-01'
-end_date = '2024-02-01'
+start_date = '2023-04-06'
+end_date = '2024-08-25'
 
 df = df[(df['Fecha'] >= start_date) & (df['Fecha'] <= end_date)]
 
